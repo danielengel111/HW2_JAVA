229d7f559767fd527fc5483a81a8ee2d83b4677d
Gal Zur <galzur01@gmail.com>
2021-06-19 20:58:01 +0300

    added getters and setters

--- src/File.java
+++ src/File.java
@@ -34,7 +34,32 @@ public class File extends StorageItem
 
     public void printContent()
     {
-        System.out.println(this.getName() + " Size: " + this.getSize() + "MB Created: " + getDate());
+        System.out.println(this.getName()
+                + " Size: " + this.getSize() + "MB Created: " + getDate());
         System.out.println(this.content);
+    }
+
+    /**
+     * getter for content attribute
+     * @return the content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * set the 'extension' attribute
+     * @param extension - the new extension
+     */
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    /**
+     * set the 'content' attribute
+     * @param content - the new content
+     */
+    public void setContent(String content) {
+        this.content = content;
     }
 }
